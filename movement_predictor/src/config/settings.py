92aa2c048c6c822f0c0dad446e3ca46e6e8cba46
g0ofycat <g0ofycatbusiness@gmail.com>
2025-09-21 13:08:14 -0400

    settings

--- movement_predictor/src/config/settings.py
+++ movement_predictor/src/config/settings.py
@@ -7,9 +7,25 @@ def load_settings():
     project_root = settings_dir.parent.parent.parent
     
     settings_file = settings_dir / 'settings_data.json'
+    
     with open(settings_file, 'r') as f:
         settings = json.load(f)
-    
+
+    model_name = settings['settings']['model_name']
+    model_version = settings['settings']['model_version']
+
+    settings['settings']['movement_data_path'] = settings['settings']['movement_data_path'].format(
+        model_name=model_name, 
+        model_version=model_version
+    )
+    settings['settings']['model_data_path'] = settings['settings']['model_data_path'].format(
+        model_name=model_name, 
+        model_version=model_version
+    )
+    settings['settings']['information'] = settings['settings']['information'].format(
+        model_name=model_name
+    )
+
     info_path = settings['settings']['information']
     if not os.path.isabs(info_path):
         info_path = project_root / info_path
